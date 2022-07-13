a519a4fa3aa70fad03a53af1e6837a3573967fdf
Ben <benawhiting@gmail.com>
2022-07-13 13:24:37 -0700

    Retrieve data from popup form using JS

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// open + close controls for the book entry popup form
+
 function openForm() {
     document.getElementById("myForm").style.display = "block";
 }
@@ -5,3 +7,27 @@ function openForm() {
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
 }
+
+//
+
+let title = document.querySelector("#title");
+let author = document.querySelector("#author");
+let pages = document.querySelector("#pages");
+let read = document.querySelector("#read");
+let submitButton = document.querySelector(".btn-submit");
+let titleOutput = document.querySelector(".title-card");
+let authorOutpout = document.querySelector(".author-card");
+let pagesOutput = document.querySelector(".pages-card");
+let readOutput = document.querySelector(".read-card");
+
+submitButton.addEventListener('click', (event) => {
+    titleOutput.textContent = `Title: ${title.value}`;
+    authorOutpout.textContent = `Author: ${author.value}`;
+    pagesOutput.textContent =  `Pages: ${pages.value}`;
+    readOutput.textContent = `Read?: ${read.value}`;
+})
+
+let form = document.querySelector(".form-container");
+
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
