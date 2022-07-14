5366160f90b7b2b09161c1028e667edbb6fe777e
Ben <benawhiting@gmail.com>
2022-07-14 13:59:24 -0700

    Add feature for removing books from display + array

--- script.js
+++ script.js
@@ -36,20 +36,49 @@ const book1 = new Book('Book 1', 'Book Man', 57, 'yes');
 const book2 = new Book('Book 2', 'Book Gal', 273, 'no');
 
 // add additional cards to the board
-
 function addCard() {
-    let cardContainer = document.querySelector(".cards");
-
     for (let i = (myLibrary.length - 1); i < myLibrary.length; i++) {
+        let cardContainer = document.querySelector(".cards");
         let cards = document.createElement("div");
+        let checkbox = document.createElement("input");
+
+        cards.dataset.indexNumber = i;
+        
+        // assigning attributes to the input (checkbox) element
+        checkbox.type = "checkbox";
+        checkbox.name = "read";
+        checkbox.id = "read";
+
+        let deleteButton = document.createElement("button");
+        let label = document.createElement("label");
+        label.htmlFor = "read";
+        label.appendChild(document.createTextNode("Read?: "));
+
+        deleteButton.id = "delete-button";
+        deleteButton.textContent = "Remove Entry";
 
         justTitles = myLibrary.map(Book => Book.title);
         justAuthors = myLibrary.map(Book => Book.author);
         justPages = myLibrary.map(Book => Book.pages);
         justReads = myLibrary.map(Book => Book.read);
 
-        cards.innerText = `Title: ${justTitles[i]} \n Author: ${justAuthors[i]} \n Pages: ${justPages[i]} \n Read?: ${justReads[i]}`;
+        cards.innerText = `Title: ${justTitles[i]} \n Author: ${justAuthors[i]} \n Pages: ${justPages[i]}`;
         cardContainer.appendChild(cards);
+
+        cards.appendChild(label);
+        label.appendChild(checkbox);
+        cards.appendChild(deleteButton);
+
+        //styling for label and checkbox
+        label.style.display = "flex";
+        label.style.alignContent = "center";
+        checkbox.style.display = "flex";
+        checkbox.style.alignSelf = "center";
+
+        deleteButton.addEventListener('click', (event) => {
+            cardContainer.removeChild(cards);
+            myLibrary.splice(i, 1, "removed");
+        })
     }
 }
 
